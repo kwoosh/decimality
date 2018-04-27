903d4b7691c17b485ac3161d1a9223f5dd07e6c9
Andrew Pashinnik <tobirawork@gmail.com>
2018-04-27 23:54:48 +0300

    test(decimal): updates history test, add tests for class methods

--- test/unit/desimal.test.js
+++ test/unit/desimal.test.js
@@ -1,13 +1,29 @@
 import Decimal from '../../src/decimal'
+import isNumber from 'is-number'
 
 describe('Desimal class', () => {
     test('History mode', () => {
-        const a = new Decimal(10.23, 'history')
+        const d = new Decimal(10.23, 'history')
 
-        a.add(12)
-        a.add(0.22)
-        a.add(213.3342234)
+        d.add(12)
+        d.add(0.22)
+        d.add(213.3342234)
 
-        expect(a.history.length).toBe(4)
+        expect(d.history).toHaveLength(4)
+    })
+
+    test('Arithmetic methos', () => {
+        const d = new Decimal(10.23, 'history')
+        const methods = ['add', 'sub', 'div', 'mul']
+
+        methods.forEach(method => {
+            expect(d[method](3) instanceof Decimal).toBeTruthy()
+        })
+    })
+
+    test('Method Decimal.s() must return a number-like string', () => {
+        const d = new Decimal(10.239)
+
+        expect(isNumber(d.s())).toBeTruthy()
     })
 })
