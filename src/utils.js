403f8955d6305a0714e7b8cc3812790a24bcdb6c
Andrew Pashinnik <tobirawork@gmail.com>
2018-04-28 13:41:28 +0300

    feat(src/utils): add decimal2number, isNumber and checkIsValid functions

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,26 @@
-export function updateHistory({ method, values, result }, ctx) {
+export const decimal2number = arg => (typeof arg !== 'number' ? arg.value : arg)
+
+export const updateHistory = ({ method, values, result }, ctx) => {
     if (ctx.mode === 'history') {
         ctx.history.push({ date: Date.now(), result, values, method })
     }
+}
+
+export const checkIsValid = value => {
+    if (!isNumber(value)) {
+        throw new Error(`Passed value ${value} doesn't seems to be number-like`)
+    }
+}
+
+export const isNumber = n => {
+    let number = Number(n)
+
+    if (number - number !== 0) return false
+    if (number === n) return true
+    if (typeof n === 'string') {
+        if (number === 0 && n.trim() === '') return false
+        else return true
+    }
+
+    return false
 }
