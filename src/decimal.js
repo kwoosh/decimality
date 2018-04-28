b4607050bd4356fbee65a2e82c406f89901407ca
Andrew Pashinnik <tobirawork@gmail.com>
2018-04-28 13:42:31 +0300

    feat(src/decimal): use checkIsValid in calculate and constuctor

--- src/decimal.js
+++ src/decimal.js
@@ -1,8 +1,10 @@
 import * as arithmetic from './arithmetic'
-import { updateHistory } from './utils'
+import { updateHistory, decimal2number, checkIsValid } from './utils'
 
 class Decimal {
-    constructor(value, mode = 'default') {
+    constructor(value = 0, mode = 'default') {
+        checkIsValid(value)
+
         this.value = arithmetic.decimalify(Number(value))
         this.mode = mode
         this.history = []
@@ -10,10 +12,10 @@ class Decimal {
         updateHistory({ method: 'init', values: [] }, this)
     }
 
-    calculate(numeric, method) {
-        numeric = numeric instanceof Decimal ? numeric.value : numeric
+    calculate(n, method) {
+        checkIsValid(n)
 
-        const values = [this.value, numeric]
+        const values = [this.value, decimal2number(Number(n))]
         const result = arithmetic[method](...values)
 
         updateHistory({ method, values, result }, this)
@@ -23,23 +25,23 @@ class Decimal {
         return this
     }
 
-    add(numeric) {
-        return this.calculate(numeric, 'add')
+    add(n) {
+        return this.calculate(n, 'add')
     }
 
-    sub(numeric) {
-        return this.calculate(numeric, 'subtract')
+    sub(n) {
+        return this.calculate(n, 'subtract')
     }
 
-    div(numeric) {
-        return this.calculate(numeric, 'divide')
+    div(n) {
+        return this.calculate(n, 'divide')
     }
 
-    mul(numeric) {
-        return this.calculate(numeric, 'multiply')
+    mul(n) {
+        return this.calculate(n, 'multiply')
     }
 
-    s() {
+    get s() {
         return String(this.value.toFixed(2))
     }
 }
