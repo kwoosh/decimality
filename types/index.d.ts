be9a0208537477cfa62e2c5f9c97776e2353e0c4
Andrew Pashinnik <tobirawork@gmail.com>
2018-05-09 16:17:59 +0300

    update(types): export all as a namespace

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,31 +1,39 @@
-export function decimalify(value: number): number
-export function add(left: number, right: number): number
-export function subtract(left: number, right: number): number
-export function divide(left: number, right: number): number
-export function multiple(left: number, right: number): number
-export function d(...args: any[]): Decimal
+declare namespace decimality {
+    function decimalify(value: number): number
 
-export type Mode = string | 'default' | 'history'
+    function add(left: number, right: number): number
+    function subtract(left: number, right: number): number
+    function divide(left: number, right: number): number
+    function multiple(left: number, right: number): number
 
-export type HistoryPoint = {
-    date: Date
-    values: number[]
-    method: string
-    result: number
+    function d(...args: any[]): Decimal
+
+    type Mode = string | 'default' | 'history'
+
+    interface HistoryPoint {
+        date: Date
+        values: number[]
+        method: string
+        result: number
+    }
+
+    class Decimal {
+        constructor(value?: number | string, mode?: Mode)
+
+        readonly history: HistoryPoint[]
+
+        value: number
+        mode: Mode
+
+        calc(n: number, method: string): Decimal
+
+        add(n: number): Decimal
+        sub(n: number): Decimal
+        mul(n: number): Decimal
+        div(n: number): Decimal
+
+        s(): string
+    }
 }
 
-export declare class Decimal {
-    constructor(value?: number | string, mode?: Mode)
-
-    readonly history: HistoryPoint[]
-    readonly s: string
-
-    value: number
-    mode: Mode
-
-    add(n: number): Decimal
-    sub(n: number): Decimal
-    mul(n: number): Decimal
-    div(n: number): Decimal
-    calc(n: number, method: string): Decimal
-}
+export default decimality
